85977a299ce13991f3c147de9e013123ca25b5c6
Chrro1999 <christian.bolanos@uisek.edu.ec>
2026-05-17 17:18:47 -0500

    Fix: Forzando lectura limpia de variables de entorno de PostgreSQL

--- tienda/settings.py
+++ tienda/settings.py
@@ -66,13 +66,13 @@ if os.getenv('DB_NAME'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-        }
+            'NAME': os.getenv('DB_NAME', 'coindecor_db'),
+            'USER': os.getenv('DB_USER', 'postgres'),
+            'PASSWORD': os.getenv('DB_PASSWORD', 'password2026'),
+            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+            'PORT': os.getenv('DB_PORT', '5432'),
     }
+}
 else:
     DATABASES = {
         'default': {

1404a86e0023c22cd0570e27ae8428c946cd3863
Chrro1999 <christian.bolanos@uisek.edu.ec>
2026-05-22 11:31:24 -0500

    UPDATE: Velocidad en carga de productos

--- catalogo/views.py
+++ catalogo/views.py
@@ -4,12 +4,13 @@ from .models import Productos, Categoria
 from .serializers import ProductosSerializers, CategoriaSerializer
 
 class ProductoViewSet(viewsets.ModelViewSet):
-    queryset = Productos.objects.all()
+    # OPTIMIZADO: select_related une productos y categorías en una sola consulta rápida
+    queryset = Productos.objects.select_related('categoria').all()
     serializer_class = ProductosSerializers
+    # Con esto permito que cualquiera vea, pero solo yo puedo editar (Movido al lugar correcto)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-
-# Con esto permito que cualquiera vea, pero solo yo puedo editar
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
